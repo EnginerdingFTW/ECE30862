93e56ef18d8ca8dd25cbbfad051d15d5642b3b5e
Taylor Lipson <tlipson@purdue.edu>
2016-10-23 14:58:19 -0400

    Committing
    huehuehue

---
+++ TestingKeyListener/src/FullScreenFrame.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.Dimension;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class FullScreenFrame extends JFrame {
+	
+
+	private static final long DEMO_TIME = 5000;
+	
+	public void run(DisplayMode displayMode)
+	{
+		setBackground(Color.blue);
+		setForeground(Color.white);
+		setFont(new Font("Dialog", 0, 24));
+		
+		SimpleScreenManager screen = new SimpleScreenManager();
+		
+	}
+	
+
+}
