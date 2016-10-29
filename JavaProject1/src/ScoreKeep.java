36ee62b01b6a453a7aea36e9d764ad63ae86d97d
chuppster <chuppa@purdue.edu>
2016-10-29 16:22:26 -0400

    Made changes to score

--- JavaProject1/src/ScoreKeep.java
+++ JavaProject1/src/ScoreKeep.java
@@ -3,18 +3,15 @@ import java.awt.*;
 public class ScoreKeep{
 	
 	public static String Score;
-	private ScoreDrawer ScrDraw = new ScoreDrawer();
 	
 	public ScoreKeep()
 	{
-		Score = "1";
-		//ScrDraw.draw();
+		Score = "0";
 	}
 	
 	public void IncreaseScore()
 	{
-		Score = Integer.toString(Integer.parseInt(Score) + 1);
-		//ScrDraw.draw();
+		Score = Integer.toString(Integer.parseInt(Score) + 100);
 	}
 	
 }
